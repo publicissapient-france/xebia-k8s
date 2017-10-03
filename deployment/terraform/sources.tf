dc0a771d321fecbd2b98b3ce360912b851b05e15
Thomas Auffredou <tauffredou@xebia.fr>
2017-10-03 22:58:11 +0200

    Store state on s3

--- deployment/terraform/sources.tf
+++ deployment/terraform/sources.tf
@@ -2,3 +2,11 @@ resource "aws_key_pair" "default" {
   public_key = "${file("${var.config_root_path}/${var.aws_cluster_name}/ssh/rsakey.pub")}"
   key_name_prefix = "${var.aws_cluster_name}"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "${var.s3_bucket}"
+    key    = "${var.aws_cluster_name}"
+    region = "${var.AWS_DEFAULT_REGION}"
+  }
+}
